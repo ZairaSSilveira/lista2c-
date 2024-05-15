ed979c0f3972c9d584240f1534d0d0cdce36ebb5
Zaira <133662712+ZairaSSilveira@users.noreply.github.com>
2024-05-15 13:15:27 -0300

    Initial commit

---
+++ lista2c##/Program.cs
@@ -0,0 +1,32 @@
+﻿////int quantidadeSorvete = 30; // Você pode alterar este número
+//double preco;
+
+//if (quantidadeSorvete <= 25)
+//{
+//    preco = 1.50;
+//}
+//else
+//{
+//    preco = 1.25;
+//}
+
+//double totalPagar = quantidadeSorvete * preco;
+//Console.WriteLine($"Total a pagar: R$ {totalPagar:F2}");
+
+int numero = 15; // Altere este número para testar outras condições
+int resultado;
+
+if (numero > 10)
+{
+    resultado = numero + 10 * 2;
+}
+else
+{
+    resultado = numero + 9 * 3 - 8;
+}
+
+Console.WriteLine($"Resultado: {resultado}");
+
+
+
+
